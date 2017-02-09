5d7ed161541d36b40389b5afec56724d1d6c8e2f
benjer3 <benjer3@outlook.com>
2017-02-08 19:44:40 -0500

    Add project files.

---
+++ CSC455RecipeManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CSC455RecipeManager.Global" Language="C#" %>
---
+++ CSC455RecipeManager/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSC455RecipeManager.Startup))]
+namespace CSC455RecipeManager
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}

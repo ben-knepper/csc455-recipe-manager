8e770556ee7314ba138473238c9b8c0a7f899ca7
benjer3 <benjer3@outlook.com>
2017-03-30 17:24:34 -0400

    Added InsertUsers.sql

--- CSC455RecipeManager/SQL/Initialization/InitDatabase.sql
+++ CSC455RecipeManager/SQL/Initialization/InitDatabase.sql
@@ -1,8 +1,7 @@
 SOURCE CreateTables.sql;
 SOURCE CreateRecipeTriggers.sql;
 SOURCE UserProcedures.sql;
+SOURCE InsertUsers.sql;
 SOURCE InsertMeasurements.sql;
 SOURCE InsertRecipes.sql;
 SOURCE InsertRecipeParts.sql;
-
-CALL CreateUser("user", "password");
---
+++ CSC455RecipeManager/SQL/Initialization/InsertUsers.sql
@@ -0,0 +1,5 @@
+CALL CreateUser("user", "password");
+CALL CreateUser("bob", "p4ssw0rd");
+CALL CreateUser("sally", "B9PCBmw7]8%@");
+CALL CreateUser("randall", "correct horse battery staple");
+CALL CreateUser("l337 h4x0r", "1234");
